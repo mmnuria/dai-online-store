68903632c4032b2297fc1b3b008134750b64852e
mmnuria <mmnuria@correo.ugr.es>
2024-10-24 13:00:41 +0200

    basic part of home.html and base.html created

--- routes/router_tienda.js
+++ routes/router_tienda.js
@@ -2,16 +2,32 @@ import express from "express";
 import Productos from "../model/productos.js";
 const router = express.Router();
       
-router.get('/home', async (req, res)=>{
-  try {
-    const productos = await Productos.find({})   // todos los productos
-    res.render('home.html', { productos })    // ../views/portada.html, 
+// router.get('/home', async (req, res)=>{
+//   try {
+//     const productos = await Productos.find({})   // todos los productos
+//     res.render('home.html', { productos })    // ../views/portada.html, 
 
-    //res.render('home.html',{p1,p2,p3})
-  } catch (err) {                                // se le pasa { productos:productos }
-    res.status(500).send({err})
+//     //res.render('home.html',{p1,p2,p3})
+//   } catch (err) {                                // se le pasa { productos:productos }
+//     res.status(500).send({err})
+//   }
+// })
+
+router.get('/home', async (req, res) => {
+  try {
+    // Obtener productos destacados
+
+    const joya = await Productos.findOne({ category: "jewelery" }).lean();
+    const electronica = await Productos.findOne({ category: "electronics" }).lean();
+    const ropaMujer = await Productos.findOne({ category: "women's clothing" }).lean();
+    const ropaHombre = await Productos.findOne({ category: "men's clothing" }).lean();
+
+    res.render('home.html', { joya, electronica, ropaMujer, ropaHombre });
+  } catch (error) {
+    console.error('Error al obtener los productos:', error);
+    res.status(500).send('Error del servidor');
   }
-})
+});
 
 // ... más rutas aquí
 
